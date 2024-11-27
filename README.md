6bd705b4502b2a5b1fd926905b6ddeccc45e4a87
Giles Westwood <giles.westwood@gmail.com>
2024-11-27 11:31:03 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,57 @@
+<div align="center">
+
+# asdf-p2cli [![Build](https://github.com/zufardhiyaulhaq/asdf-p2cli/actions/workflows/build.yml/badge.svg)](https://github.com/zufardhiyaulhaq/asdf-p2cli/actions/workflows/build.yml) [![Lint](https://github.com/zufardhiyaulhaq/asdf-p2cli/actions/workflows/lint.yml/badge.svg)](https://github.com/zufardhiyaulhaq/asdf-p2cli/actions/workflows/lint.yml)
+
+
+[p2cli](https://github.com/aquasecurity/p2cli) plugin for the [asdf version manager](https://asdf-vm.com).
+
+</div>
+
+# Contents
+
+- [Dependencies](#dependencies)
+- [Install](#install)
+- [Why?](#why)
+- [Contributing](#contributing)
+- [License](#license)
+
+# Dependencies
+
+- `bash`, `curl`, `tar`: generic POSIX utilities.
+
+# Install
+
+Plugin:
+
+```shell
+asdf plugin add p2cli https://github.com/zufardhiyaulhaq/asdf-p2cli.git
+```
+
+p2cli:
+
+```shell
+# Show all installable versions
+asdf list-all p2cli
+
+# Install specific version
+asdf install p2cli latest
+
+# Set a version globally (on your ~/.tool-versions file)
+asdf global p2cli latest
+
+# Now p2cli commands are available
+p2cli --version
+```
+
+Check [asdf](https://github.com/asdf-vm/asdf) readme for more instructions on how to
+install & manage versions.
+
+# Contributing
+
+Contributions of any kind welcome! See the [contributing guide](contributing.md).
+
+[Thanks goes to these contributors](https://github.com/zufardhiyaulhaq/asdf-p2cli/graphs/contributors)!
+
+# License
+
+See [LICENSE](LICENSE) © [Zufar Dhiyaulhaq](https://github.com/zufardhiyaulhaq/)
---
+++ lib/utils.bash
@@ -0,0 +1,85 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+GH_REPO="https://github.com/aquasecurity/p2cli"
+TOOL_NAME="p2cli"
+TOOL_TEST="p2cli --version"
+
+fail() {
+  echo -e "asdf-$TOOL_NAME: $*"
+  exit 1
+}
+
+curl_opts=(-fsSL)
+
+if [ -n "${GITHUB_API_TOKEN:-}" ]; then
+  curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
+fi
+
+list_all_versions() {
+  list_github_tags
+}
+
+list_github_tags() {
+  git ls-remote --tags --refs "$GH_REPO" |
+    grep -o 'refs/tags/.*' | cut -d/ -f3- |
+    sed 's/^v//'
+}
+
+sort_versions() {
+  sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
+    LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
+}
+
+download_release() {
+  local version filename url
+  version="$1"
+  filename="$2"
+
+  local platform
+  case "$(uname) in
+    Linux) arch=linux ;;
+    Darwin) arch=darwin ;;
+    FreeBSD) arch=freebsd ;;
+    *) arch=windows
+  esac
+
+  local arch
+  case "$(uname -m)" in
+    x86_64) arch=amd64 ;;
+    x86) arch=386 ;;
+    aarch64|arm64) arch=arm64 ;;
+  esac
+
+  url="$GH_REPO/releases/download/r${version}/p2cli_r${version}_${platform}-${arch}.tar.gz"
+
+  echo "* Downloading $TOOL_NAME release $version..."
+  curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
+}
+
+install_version() {
+  local install_type="$1"
+  local version="$2"
+  local install_path="$3/bin"
+
+  if [ "$install_type" != "version" ]; then
+    fail "asdf-$TOOL_NAME supports release installs only"
+  fi
+
+  (
+    local tool_cmd
+    tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
+
+    mkdir -p "$install_path"
+    cp -r "$ASDF_DOWNLOAD_PATH"/$tool_cmd "$install_path"
+
+    chmod +x "$install_path/$tool_cmd"
+    test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
+
+    echo "$TOOL_NAME $version installation was successful in $install_path!"
+  ) || (
+    rm -rf "$install_path"
+    fail "An error ocurred while installing $TOOL_NAME $version."
+  )
+}
